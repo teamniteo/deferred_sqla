f3a61b06af7c9e7850a84002c90eddf088c082ff
Neyts Zupan <nejc.zupan@gmail.com>
2022-01-06 21:05:21 +0000

    Rename niteoweb -> teamniteo
    Refs https://github.com/teamniteo/operations/issues/1344

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
     long_description=long_description,
     license='Apache2',
     long_description_content_type='text/markdown',
-    url='https://github.com/niteoweb/pyramid_deferred_sqla',
-    author='Niteoweb',
+    url='https://github.com/teamniteo/pyramid_deferred_sqla',
+    author='Niteo',
     author_email='info@niteo.co',
     classifiers=[  # Optional
         'Development Status :: 3 - Alpha',
